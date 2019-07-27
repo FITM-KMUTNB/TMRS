e648fc94e72fbd9729b125453ca48bc295ccb1cd
Kaow <kaowrg@gmail.com>
2019-07-27 12:19:03 +0700

    add node

--- Cooccs.py
+++ Cooccs.py
@@ -1,31 +1,50 @@
 import os
-from natsort import natsorted
-import nltk
 import sys
+from natsort import natsorted #For retrieve list file from directory
+import nltk #For Categorize And TaggingWord
+from py2neo import Graph, Node, Relationship, NodeMatcher #Working with Neo4j
 
-path = "diseasesfulltextswiki/"
+
+#Document Directory
+path = "Document/Test/"
 diseasedir = natsorted(os.listdir(path))
+#Neo4j Database
+print('Connect to database...', end="", flush=True)
+graph = Graph(password = "tmrs_1234")
+try:
+    graph.run("Match () Return 1 Limit 1")
+    print(' Success !')
+except Exception:
+    print(' Failed !')
 
+matcher = NodeMatcher(graph)
+
+#Main menu
 def main():
+    
     print("===============================================")
     print("Thai Traditional Medical Recommendation System.")
     print("===============================================")
     choice = input("""
-    (C) ategorize And Tagging Word
-    (E) xit
-    Please Enter Your Choice :: """)
+(C)ategorize And Tagging Word
+(T)he Co-Occurrence Graph
+(E)xit
+\n
+Please Enter Your Choice :: """)
 
     if choice == "C" or choice == "c":
         CategorizeAndTaggingWord()
+    elif choice == "T" or choice == "t":
+        CreateCoocurrenceGraph()
     elif choice == "E" or choice == "e":
         sys.exit
 
 def CategorizeAndTaggingWord():
     for name in diseasedir:
         try:       
-            with open(path+"/"+name, "r", encoding="latin-1") as f:
-                text = nltk.word_tokenize(f.read())
-                result = nltk.pos_tag(text)
+            with open(path+"/"+name, "r", encoding="latin-1") as f: # open txt file from directory and keep in f variable
+                text = nltk.word_tokenize(f.read()) # Retrieve word from document
+                result = nltk.pos_tag(text) # Tagging word Pos
                 print(result)
                 print("\n")
                 print(name) 
@@ -35,6 +54,84 @@ def CategorizeAndTaggingWord():
             if exc.errno != errno.EISDIR:
                 raise
     main()
+
+def CreateCoocurrenceGraph():
+    print("\n")
+    print("-> Start Create Co-Occurrence Graph")
+    SentenceVector = []
+    
+    for name in diseasedir:
+        print("-> Retrieve Document file")
+        try:       
+            with open(path+"/"+name, "r", encoding="latin-1") as f: # open txt file from directory and keep in f variable
+                
+                for text in f:
+                    
+                    VectorRow = []
+                    sentence = nltk.word_tokenize(text) # Retrieve word from document per sentence
+                    #print(text) # Retrieve word from document
+                    
+                    # Retrieve word from each sentence                
+                    for word in sentence:
+                        VectorRow.append(word)
+                    # Store Sentence to list
+                    SentenceVector.append(VectorRow)
+
+                # Display Output
+                """
+                SentenceRow = 1
+                for DisplaySV in SentenceVector:
+                    print(SentenceRow, "::", DisplaySV)
+                    SentenceRow += 1
+                """
+                print("Read text file [", name, "] ... Done") 
+                
+                CreateNodeAndRelation(SentenceVector)
+        except IOError as exc:
+            print("Error")
+            if exc.errno != errno.EISDIR:
+                raise
+    main()
+
+def CreateNodeAndRelation(SentenceVector):
+    print("\n")
+    print("-> Create Node And Relation")
+    tx = graph.begin() 
+    print("in transaction of database")    
+    for Words in SentenceVector:
+        # Seperate word|POS
+        tempword = ""
+        temppos = ""
+        for word in Words:
+            if "|" in word:
+                tempword, temppos = word.split("|")
+            else:
+                tempword = word
+            f_flag = 0
+            # checking if the node is already present
+            for record in matcher.match("SINGLE_NODE"):
+                
+                wordnode = record["name"]
+                
+                if wordnode == tempword:
+                    occur = record["occur"]
+                    occur += 1
+                    record["occur"] = occur
+                    tx.push(record)
+                    f_flag = 1
+                    print(wordnode, " node was already present. Count updated.")    
+            
+            if f_flag != 1:
+                wordnode = Node("SINGLE_NODE", name=tempword, occur=1, pos=temppos) 
+                tx.create(wordnode)
+                print(wordnode, " node added.")  
+          
+            
+    
+    tx.commit()
+    
+    
+
 
 if __name__ == "__main__":
     main()
--- Test.py
+++ Test.py
@@ -1,1 +1,17 @@
-print("Hello kaow")
+from py2neo import Graph,Node, NodeMatcher
+graph = Graph(password = "tmrs_1234")
+matcher = NodeMatcher(graph)
+result = matcher.match("SINGLE_NODE", name="KEY")
+a = Node("SINGLE_NODE", name="KEY")
+
+
+for rel in matcher.match("SINGLE_NODE"):
+    print(rel)
+    print("name:", rel["name"])
+    print("occur:", rel["occur"])
+    print("pos:", rel["pos"])
+
+def node_id(node):
+
+    return None
+#print(graph.nodes[124]) #fetch by id
