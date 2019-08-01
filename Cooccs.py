e86b5d4259d4bc2d44d0f1309a022ca9f2ee1570
Kaow <kaowrg@gmail.com>
2019-08-01 21:38:33 +0700

    add relationship

--- Cooccs.py
+++ Cooccs.py
@@ -2,15 +2,15 @@ import os
 import sys
 from natsort import natsorted #For retrieve list file from directory
 import nltk #For Categorize And TaggingWord
-from py2neo import Graph, Node, Relationship, NodeMatcher #Working with Neo4j
+from py2neo import Graph, Node, Relationship, NodeMatcher, RelationshipMatcher #Working with Neo4j
 
 
 #Document Directory
-path = "Document/Test/"
+path = "Document/output_allWords_/"
 diseasedir = natsorted(os.listdir(path))
 #Neo4j Database
 print('Connect to database...', end="", flush=True)
-graph = Graph(password = "tmrs_1234")
+graph = Graph(password = "tmrs_2019")
 try:
     graph.run("Match () Return 1 Limit 1")
     print(' Success !')
@@ -18,6 +18,7 @@ except Exception:
     print(' Failed !')
 
 matcher = NodeMatcher(graph)
+relmatcher = RelationshipMatcher(graph)
 
 #Main menu
 def main():
@@ -60,78 +61,146 @@ def CreateCoocurrenceGraph():
     print("-> Start Create Co-Occurrence Graph")
     SentenceVector = []
     
-    for name in diseasedir:
+    for DocName in diseasedir:
         print("-> Retrieve Document file")
         try:       
-            with open(path+"/"+name, "r", encoding="latin-1") as f: # open txt file from directory and keep in f variable
-                
-                for text in f:
+            with open(path+"/"+DocName, "r", encoding="latin-1") as f: # open txt file from directory and keep in f variable
+                Sline = 1
+                AllSline = len(open(path+"/"+DocName).readlines())
+                for text in f: # Read Document of each line(Sentence) --> Store in text
                     
                     VectorRow = []
-                    sentence = nltk.word_tokenize(text) # Retrieve word from document per sentence
-                    #print(text) # Retrieve word from document
+                    sentence = text.split(" ") # Seperate word from each line and store in sentence valiable {w1,w2,w3}
+                                       
                     
-                    # Retrieve word from each sentence                
+                    # Retrieve word from each sentence               
                     for word in sentence:
-                        VectorRow.append(word)
-                    # Store Sentence to list
-                    SentenceVector.append(VectorRow)
+                        VectorRow.append(word) # Words form each sentence store to VectorRow[] {Word|POS}
+                    
+                    # Sent Sentence to Database 
+                    CreateNodeAndRelation(VectorRow, DocName, Sline, AllSline)
+                    Sline +=1
+                    
 
-                # Display Output
-                """
-                SentenceRow = 1
-                for DisplaySV in SentenceVector:
-                    print(SentenceRow, "::", DisplaySV)
-                    SentenceRow += 1
-                """
-                print("Read text file [", name, "] ... Done") 
+                print("Read text file [", DocName, "] ... Done\n") 
                 
-                CreateNodeAndRelation(SentenceVector)
+                
         except IOError as exc:
             print("Error")
             if exc.errno != errno.EISDIR:
                 raise
     main()
 
-def CreateNodeAndRelation(SentenceVector):
-    print("\n")
-    print("-> Create Node And Relation")
-    tx = graph.begin() 
-    print("in transaction of database")    
-    for Words in SentenceVector:
-        # Seperate word|POS
-        tempword = ""
-        temppos = ""
-        for word in Words:
-            if "|" in word:
-                tempword, temppos = word.split("|")
-            else:
-                tempword = word
-            f_flag = 0
-            # checking if the node is already present
-            for record in matcher.match("SINGLE_NODE"):
-                
-                wordnode = record["name"]
-                
-                if wordnode == tempword:
-                    occur = record["occur"]
-                    occur += 1
-                    record["occur"] = occur
-                    tx.push(record)
-                    f_flag = 1
-                    print(wordnode, " node was already present. Count updated.")    
-            
-            if f_flag != 1:
+def CreateNodeAndRelation(SentenceVector, DocName, Sline, AllSline):
+        
+    tempS = []
+    # Seperate word|POS
+    tempword = ""
+    temppos = ""
+    for word in SentenceVector:
+        if "|" in word:
+            tempword, temppos = word.split("|")
+        else:
+            tempword = word
+            temppos = None
+        f_flag = 0
+        tempS.append(tempword)
+        # checking if the node is already present
+        for record in matcher.match("SINGLE_NODE"):
+               
+            wordnode = record["name"]
+            # if node already present --> Update Count!!    
+            if wordnode == tempword:
+                occur = record["occur"]
+                occur += 1
+                record["occur"] = occur
+                tx = graph.begin() 
+                tx.push(record)
+                tx.commit()
+                f_flag = 1
+                print("[", DocName," [",Sline,"/",AllSline,"] \"", tempword, "\" node was already present. Count updated.")    
+        # Create new node   
+        if f_flag != 1:
+            if temppos != None:
                 wordnode = Node("SINGLE_NODE", name=tempword, occur=1, pos=temppos) 
+                tx = graph.begin() 
                 tx.create(wordnode)
-                print(wordnode, " node added.")  
-          
-            
-    
-    tx.commit()
-    
-    
+                tx.commit()
+                print("[", DocName," [",Sline,"/",AllSline,"] \"", tempword, "\" node added.")  
+            else:
+                wordnode = Node("SINGLE_NODE", name=tempword, occur=1) 
+                tx = graph.begin() 
+                tx.create(wordnode)
+                tx.commit()
+                print("[", DocName," [",Sline,"/",AllSline,"] \"", tempword, "\" node added.")  
+
+      
+
+    # Add relationship between node.
+    for p in range(len(tempS)):
+        
+        for q in range(p+1, len(tempS)):
+                        
+            rel_found = False
+            # Check if node the same node
+            if not (tempS[p] == tempS[q]):
+                rel_found = False
+                
+                #Get Node ID
+                nodeid1 = 0
+                for nid in graph.run("match (n:SINGLE_NODE {name: '"+tempS[p]+"'}) return id(n) as NODEID"):
+                     nodeid1 = nid["NODEID"]
+                     
+                nodeid2 = 0
+                for nid in graph.run("match (n:SINGLE_NODE {name: '"+tempS[q]+"'}) return id(n) as NODEID"):
+                    nodeid2 = nid["NODEID"]
+                
+                n1 = graph.nodes[nodeid1]
+                n2 = graph.nodes[nodeid2]
+                
+                # If Relationship already exist --> Update Count!!
+                if relmatcher.match(nodes=(n1, n2), r_type="IS_CONNECTED"):
+                    for rel in relmatcher.match(nodes=(n1, n2), r_type="IS_CONNECTED"):
+                        count = rel["count"]
+                        count += 1
+                        rel["count"] = count
+                        tx = graph.begin()
+                        tx.push(rel)
+                        tx.commit()
+                        print("[", DocName," [",Sline,"/",AllSline,"] Relation already existed between nodes \"", tempS[p], "\" and \"", tempS[q], "\" Count updated.")
+                    rel_found = True
+
+                if relmatcher.match(nodes=(n2, n1), r_type="IS_CONNECTED"):
+                    for rel in relmatcher.match(nodes=(n2, n1), r_type="IS_CONNECTED"):
+                        count = rel["count"]
+                        count += 1
+                        rel["count"] = count
+                        tx = graph.begin()
+                        tx.push(rel)
+                        tx.commit()
+                        print("[", DocName," [",Sline,"/",AllSline,"] Relation already existed between nodes \"", tempS[p], "\" and \"", tempS[q], "\" Count updated.")
+                    rel_found = True
+                        
+                # Create new relationship
+                if not rel_found:
+                    n1id = 0
+                    n2id = 0
+                    for nid in graph.run("match (n:SINGLE_NODE {name: '"+tempS[p]+"'}) return id(n) as NODEID"):
+                        n1id = nid["NODEID"]
+                        
+                    for nid in graph.run("match (n:SINGLE_NODE {name: '"+tempS[q]+"'}) return id(n) as NODEID"):
+                        n2id = nid["NODEID"]
+                        
+                    n1 = graph.nodes[n1id]
+                    n2 = graph.nodes[n2id]
+                    relationship = Relationship(n1, "IS_CONNECTED", n2, count=1, dice=0, cost=0)
+                    tx = graph.begin()
+                    tx.create(relationship)
+                    tx.commit()
+                    print("[", DocName," [",Sline,"/",AllSline,"] Relation inserted with nodes \"", tempS[p], "\" and \"", tempS[q],"\"")
+                    
 
 
+    
 if __name__ == "__main__":
     main()
--- Test.py
+++ Test.py
@@ -1,17 +1,97 @@
-from py2neo import Graph,Node, NodeMatcher
-graph = Graph(password = "tmrs_1234")
+from py2neo import Graph,Node, NodeMatcher, Relationship,RelationshipMatcher,Record
+graph = Graph(password = "tmrs_2019")
 matcher = NodeMatcher(graph)
+relmatcher = RelationshipMatcher(graph)
+
+count = len(open("Document/output_allWords_/2.txt.txt.s_allWords_sentences.txt").readlines())
+print(count)
+nodeid1 = 0
+for nid in graph.run("match (n:SINGLE_NODE {name: 'KEY'}) return id(n) as NODEID"):
+        nodeid1 = nid["NODEID"]
+        print(nodeid1)
+    
+nodeid2 = 0
+for nid in graph.run("match (n:SINGLE_NODE {name: 'ROOM'}) return id(n) as NODEID"):
+        nodeid2 = nid["NODEID"]
+        print(nodeid2)
+
+n1 = graph.nodes[nodeid1]
+n2 = graph.nodes[nodeid2]
+
+ckrel = relmatcher.match(nodes=(n1, n2), r_type="IS_CONNECTED")
+print(list(ckrel))
+relfound = False
+if relmatcher.match(nodes=(n2, n1), r_type="IS_CONNECTED"):
+    print("Match!!")
+    relfound = True
+if not relfound:
+    print("IN!!")
+
 result = matcher.match("SINGLE_NODE", name="KEY")
 a = Node("SINGLE_NODE", name="KEY")
+print("result->", result)
+print("a->", a)
 
-
-for rel in matcher.match("SINGLE_NODE"):
+"""for rel in matcher.match("SINGLE_NODE"):
     print(rel)
     print("name:", rel["name"])
     print("occur:", rel["occur"])
     print("pos:", rel["pos"])
+"""
+"""
+num=1
+VectorRow =[]
+for x in range(len(matcher.match("SINGLE_NODE"))):
+    VectorRow.append(num)
+    print(x)
+    num+=1
 
-def node_id(node):
+for i in range(len(VectorRow)):
+    print("No.", i),
+    print(VectorRow[i])
 
-    return None
+
+a = "me"
+b = "me"
+
+if not (a == b):
+    print("Match")
+"""
+
+tx = graph.begin() 
+
+
+create = True
+if not create:
+    a = Node("SINGLE_NODE", name="a") 
+    tx.create(a)
+    b = Node("SINGLE_NODE", name="b") 
+    tx.create(b)
+    c = Node("SINGLE_NODE", name="c") 
+    tx.create(c)
+    d = Node("SINGLE_NODE", name="d") 
+    tx.create(d)
+tx.commit()
+
+    
+tx = graph.begin()
+rel = True
+if not rel:
+    nodeid1 = 0
+    for nid in graph.run("match (n:SINGLE_NODE {name: 'a'}) return id(n) as NODEID"):
+         nodeid1 = nid["NODEID"]
+    
+    nodeid2 = 0
+    for nid in graph.run("match (n:SINGLE_NODE {name: 'b'}) return id(n) as NODEID"):
+        nodeid2 = nid["NODEID"]
+    a = graph.nodes[nodeid1]
+    b = graph.nodes[nodeid2]
+    ab = Relationship(a, "IS_CONNECTED", b, count=2)
+    tx.create(ab)
+    print("Create rel success")
+tx.commit()
+    
 #print(graph.nodes[124]) #fetch by id
+
+
+
