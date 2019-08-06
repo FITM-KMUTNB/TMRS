733fab572c5f19ada40baf5f1471451243308bb4
WinWin12345 <withawin5036@hotmail.co.th>
2019-08-06 16:25:27 +0700

    ์NX

--- NetworkX.py
+++ NetworkX.py
@@ -1,5 +1,5 @@
 import networkx as nx
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 g=nx.Graph()
 
@@ -7,11 +7,17 @@ g.add_node(2)
 g.add_node(5)
 
 g.add_edge(2,5)
-
 g.add_edge(4,1)
 
+g.add_edges_from([(2,5),(1,3)])
+
+print nx.info(g)
 
 
 nx.draw(g)
 
 plt.show()
+
+
+
+
