417f0510f7bfa8a3a2f3974b1325d2ce84a8ba85
WinWin12345 <withawin5036@hotmail.co.th>
2019-08-27 13:45:14 +0700

    t

--- Pytest.py
+++ Pytest.py
@@ -1,38 +1,12 @@
-import os
-import glob
-from fuzzywuzzy import fuzz
+file = open("Result.txt","w") 
+r = 1
+query = ["a", "b"]
 
-path = "Document/corpus 226/Tag"
+result = str(r)+" : ""%s\n" % query +"\n"
+file.write(result) 
+r = 2
+query = ["c", "d"]
+result = str(r)+" : ""%s\n" % query +"\n"
+file.write(str(r)+" : ""%s\n" % query +"\n") 
 
-a = fuzz.ratio("new y YANKEES".lower(), "NEW YORK YANKEES".lower())
-print(a)
-
-def listfile(path):
-    
-    os.chdir(path)
-    doc_no = 1
-    for DocName in glob.glob("*.txt"):
-        print(doc_no," :: "+DocName)
-        listsentence(DocName)
-        doc_no+=1
-     
-    
-
-def listsentence(fname):
-    c_file = open(fname, 'r', encoding="latin-1")
-    disease = {}
-    for sents in c_file:
-        nline = sents.replace("{", "")
-        pline = nline.replace("}", "")
-        pline = pline.rstrip('\n')
-        words = pline.split(",")
-        for d in words:
-            n, s = d.split("=")
-            disease[n.lower()] = s
-        
-    #print(disease)    
-    
-    c_file.close()
-
-listfile(path)
-    
+file.close() 
