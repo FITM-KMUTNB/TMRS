5029f9f63b91542368f937698e16af7c86bf7b67
Kaow <kaowrg@gmail.com>
2019-09-13 21:58:11 +0700

    cleanword

--- Cleantext.py
+++ Cleantext.py
@@ -6,21 +6,26 @@ import glob
 import chardet
 import PreTextProcessing as pt
 import Tokenization as tk
+import inflect
+p = inflect.engine()
+Taglist = []
+DiseaseName = []
 def dir():
-    path = "C:/Users/Kaow/Documents/Project/TMRS/Document/corpus221/cleanword/ADHD.txt.txt.s_nouns_sentences_untagged.txt"
-    #os.chdir(path)
-    #for file in glob.glob("*.txt"):
-        #print("file name: ", file)
-    readline(path)
+    path = "C:/Users/Kaow/Documents/Project/TMRS/Document/corpus221/"
+    os.chdir(path)
+    for file in glob.glob("*.txt"):
+        print("file name: ", file)
+        readline(file)
 
 def readline(file):
     #Detect file encoding type of file
+
     rawdata = open(file, 'rb').read()
     FileCode = chardet.detect(rawdata)
     Encode = FileCode['encoding']
     
     c_file = open(file, 'r', encoding=Encode)
-    file = open("C:/Users/Kaow/Documents/Project/TMRS/Document/corpus221/cleanword2/test.txt","w", encoding="utf-8")
+    file = open("C:/Users/Kaow/Documents/Project/TMRS/Document/corpus221/cleanword2/"+file,"w", encoding="utf-8")
     for c in c_file:
         cleantxt(c, file)
     file.close()     
@@ -28,13 +33,41 @@ def readline(file):
 
 def cleantxt(line, cleanfile):
   
-    #word_list = tk.TokenizeMultiWord(line, Taglist)
-    word_list = line.split()
-    for word in word_list:
-        if not word.isnumeric() and len(word) > 1 and word in words.words():
-            cleanfile.write(word)
-        cleanfile.write(" ")
-    cleanfile.write("\n") 
+    word_list = tk.TokenizeMultiWord(line, Taglist)
     
+    for word in word_list:
+  
+        if (not word.isnumeric() and len(word) > 1 and word in words.words()) or (word in DiseaseName):
+            cleanfile.write(word)
+            cleanfile.write(" ")
+        else:
+            if p.singular_noun(word):
+                cleanfile.write(word)
+                cleanfile.write(" ")
 
+
+    cleanfile.write("\n") 
+#Prepare keyword to matching with word in text file
+def WordTags():
+    global Taglist
+    
+    os.chdir("Document/corpus221/Wiki")
+    print("Prepare Disease Name Tags.")
+    for file in glob.glob("*.txt"): 
+        #Detect file encoding type of file
+        rawdata = open(file, 'rb').read()
+        FileCode = chardet.detect(rawdata)
+        Encode = FileCode['encoding']
+        print(Encode)
+        #For Check Disease Name In Sentence
+        Text_file = open(file, 'r', encoding=Encode) 
+        firstLine = Text_file.readline() # Read first line in each disease document to get disease name.
+        removen = firstLine.replace("\n", "")
+        DiseaseTag = removen.lower().split()
+        Taglist.append(DiseaseTag)
+        #For Tag Disease Node
+        replaceu = removen.lower().replace(" ", "_")
+        DiseaseName.append(replaceu)
+      
+WordTags()
 dir()
