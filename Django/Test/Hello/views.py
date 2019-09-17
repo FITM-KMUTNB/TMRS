29f3390d9bea02cc7acf3d8402477cc4411592fa
WinWin12345 <withawin5036@hotmail.co.th>
2019-09-17 15:11:54 +0700

    t

--- Django/Test/Hello/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def home(request,*args, **kwargs):
-    print(args, kwargs)
-    print(request.user)
-    #return HttpResponse("<h1>eiei</h1>")
-    return render(request, "index.php",{})
