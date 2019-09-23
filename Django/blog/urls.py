d287c1c1d9b3dc204e771eb7bbe0a6160a938514
WinWin12345 <withawin5036@hotmail.co.th>
2019-09-23 23:33:26 +0700

    web

--- Django/blog/urls.py
+++ Django/blog/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from blog.views import HomeView
-
+from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^document/$', views.document, name='document'),
+    url(r'^neighbor/$', views.neighbor, name='neighbor'),
+    
     
 ]
 
