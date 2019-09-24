d0e78d07325820dedbafa8eb4ae6dedd3ea1eecb
WinWin12345 <withawin5036@hotmail.co.th>
2019-09-24 17:08:21 +0700

    testcreate

---
+++ TestCreateGraph.py
@@ -0,0 +1,12 @@
+import Library as ct
+import networkx as nx
+def create_tmrs_graph():
+    input_text_dir = "Document/corpus221/cleantag/"
+    g_file = "Database/Pickle/221test.gpickle"
+    ct.create_graph(input_text_dir, g_file)
+
+#create_tmrs_graph()
+
+G = nx.read_gpickle("Database/Pickle/221test.gpickle")
+
+print(nx.info(G))
