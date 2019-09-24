84d70befd8da5e83f8700da23f9084ecd9470b03
WinWin12345 <withawin5036@hotmail.co.th>
2019-09-24 18:20:00 +0700

    testcreategraph

--- TestCreateGraph.py
+++ TestCreateGraph.py
@@ -5,8 +5,12 @@ def create_tmrs_graph():
     g_file = "Database/Pickle/221test.gpickle"
     ct.create_graph(input_text_dir, g_file)
 
+
 #create_tmrs_graph()
-
 G = nx.read_gpickle("Database/Pickle/221test.gpickle")
-
 print(nx.info(G))
+neighbor = G.neighbors('disease')
+
+for n in neighbor:
+    print('disease - ', n," : ", G['disease'][n]['cost'])
+
