9289a55c47b3aff1c7e298ab56b6d82156ddd3a2
Kaow <kaowrg@gmail.com>
2019-10-23 19:37:43 +0700

    edit_hop_spreading

--- Code07.py
+++ Code07.py
@@ -47,32 +47,33 @@ def spreading_activation_centroid(G, keywords):
             node_distance.append(initial)
             key_point.append([key])
 
-    while len(candidate) < 10:
-        #print("Activation round : ", activate_round)
+    next_act = 0
+    while len(candidate) < 1:
+        print("Activation round : ", activate_round)
         min_average = 999999
         centroid = ''
-        
-        
-
         for index in range(len(key_point)):
             activate_size = 1
             # initail point.
             for a in range(activate_size):
-                act_node = key_point[index].pop(0)
-                #print("Activate [", index,"]:: ", act_node)
+                act_node = key_point[index][next_act]
+                print("Activate [", index,"]:: ", act_node)
 
                 # Activate
                 related_node = nx.neighbors(G, act_node)
 
                 # iterate neighbors.
                 for r in related_node:
+                    if r in keywords:
+                        continue
+
                     if r not in key_point[index]:
                         # append node for next activation.
                         key_point[index].append(r)
                         distance_dict = node_distance[index]
                         
                         # distance from neightbor to activate node.
-                        distance_dict[r] = distance_dict[act_node] + G[act_node][r]['cost']
+                        distance_dict[r] = distance_dict[act_node] + 1
                         if r in node_count:
                             node_count[r] += 1
                             node_sum[r] += distance_dict[r]
@@ -81,20 +82,21 @@ def spreading_activation_centroid(G, keywords):
                             node_sum[r] = distance_dict[r]
                     
                     if node_count[r] == len(keywords):
-                        if G.node[r]['tag'] == 'DS':
-                            candidate[r] = node_sum[r] / len(keywords)
+                        
+                        candidate[r] = node_sum[r] / len(keywords)
                         """if min_average > candidate[r]:
                             min_average = candidate[r]
                             centroid = r"""
         #print("Candidate : ", len(candidate))
-        
+        print(node_count)
+        print(key_point)
         activate_round += 1
+        next_act += 1
     
     return dict(sorted(candidate.items(), key=operator.itemgetter(1)))
 
 # Example
-G = nx.read_gpickle("Database/Pickle/221tag.gpickle")
-keywords = ['skin', 'itch', 'headache']
+keywords = ['I', 'A', 'E']
 centroid = spreading_activation_centroid(G, keywords)
 
 for c in list(centroid.items())[:10]:
