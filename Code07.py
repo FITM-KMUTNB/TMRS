23084eb9afedd2e33cc995ad0c9dccce57460d84
Kaow <kaowrg@gmail.com>
2019-10-30 17:06:52 +0700

    hop candidate

--- Code07.py
+++ Code07.py
@@ -7,17 +7,17 @@ G = nx.Graph()
 G.add_edges_from([('A', 'B'), ('A', 'M'), ('A', 'L'), ('B', 'C'), ('B', 'D'),
                   ('B', 'N'), ('B', 'O'), ('C', 'D'), ('D', 'E'), ('D', 'O'), ('E', 'F'), ('F', 'G'), ('F', 'N'), ('G', 'H'), ('H', 'N'), ('H', 'I'), ('H', 'P'), ('P', 'O'), ('P', 'I'), ('P', 'M'), ('I', 'J'), ('J', 'K'), ('K', 'M'), ('K', 'L')])
 
-print('Shortest path from E to L is :',
-      nx.shortest_path(G, source='E', target='L'))
+#print('Shortest path from E to L is :',
+      #nx.shortest_path(G, source='E', target='L'))
 
 final_avg = 999999999.99
 final_key = ''
 # find node shortest path to all nodes
 node_allSP = dict(nx.shortest_path_length(G, weight='weight'))
-print(node_allSP)
+#print(node_allSP)
 for key in node_allSP:
     avg_nodeSP = sum(node_allSP[key].values())/(len(node_allSP[key]))
-    print('AverageSP to all nodes of :', key, ' is ', avg_nodeSP)
+    #print('AverageSP to all nodes of :', key, ' is ', avg_nodeSP)
     if final_avg > avg_nodeSP:
         final_avg = avg_nodeSP
         final_key = key
@@ -25,80 +25,48 @@ for key in node_allSP:
 print('The centroid is : ', final_key, 'the length is ', final_avg)
 
 # Draw Graph
-pos = nx.spring_layout(G)
-nx.draw(G, pos, with_labels=True, font_color='yellow', node_size=1500)
-plot.show()
+#pos = nx.spring_layout(G)
+#nx.draw(G, pos, with_labels=True, font_color='yellow', node_size=1500)
+#plot.show()
 
 
 def spreading_activation_centroid(G, keywords):
-    centroid = ''
-    key_point = [] # node point to activate and find neighbors.
-    activate_round = 1
-    candidate = dict()
-    
-    node_count = dict() # node count to all keywords
-    node_sum = dict() # node sum distance to all keywords
-    node_distance = [] # distance from node to each keywords
+    activate_list = []
+    candidate = []
+    current_hop = 0
+    node_count = dict()
 
     for key in keywords:
-            initial = dict()
-            # initail distance 
-            initial[key] = 0
-            node_distance.append(initial)
-            key_point.append([key])
+        activate_list.append([key])
 
-    next_act = 0
-    while len(candidate) < 1:
-        print("Activation round : ", activate_round)
-        min_average = 999999
-        centroid = ''
-        for index in range(len(key_point)):
-            activate_size = 1
-            # initail point.
-            for a in range(activate_size):
-                act_node = key_point[index][next_act]
-                print("Activate [", index,"]:: ", act_node)
-
-                # Activate
-                related_node = nx.neighbors(G, act_node)
-
-                # iterate neighbors.
-                for r in related_node:
-                    if r in keywords:
-                        continue
-
-                    if r not in key_point[index]:
-                        # append node for next activation.
-                        key_point[index].append(r)
-                        distance_dict = node_distance[index]
-                        
-                        # distance from neightbor to activate node.
-                        distance_dict[r] = distance_dict[act_node] + 1
-                        if r in node_count:
-                            node_count[r] += 1
-                            node_sum[r] += distance_dict[r]
-                        else:
-                            node_count[r] = 1
-                            node_sum[r] = distance_dict[r]
-                    
-                    if node_count[r] == len(keywords):
-                        
-                        candidate[r] = node_sum[r] / len(keywords)
-                        """if min_average > candidate[r]:
-                            min_average = candidate[r]
-                            centroid = r"""
-        #print("Candidate : ", len(candidate))
-        print(node_count)
-        print(key_point)
-        activate_round += 1
-        next_act += 1
     
-    return dict(sorted(candidate.items(), key=operator.itemgetter(1)))
+    while len(candidate) <= 0:
+        for circle in activate_list:
+
+            for neighbors in nx.neighbors(G, circle[current_hop]):
+                if neighbors in keywords:
+                    continue
+
+                if neighbors in node_count:
+
+                    if neighbors not in circle:
+                        circle.append(neighbors)
+                        node_count[neighbors] += 1
+            
+                else:
+                    circle.append(neighbors)
+                    node_count[neighbors] = 1
+        
+        for node in node_count:
+            if node_count[node] == len(keywords):
+                candidate.append(node)
+        current_hop += 1
+ 
+    print(candidate)       
+    
+    
 
 # Example
 keywords = ['I', 'A', 'E']
-centroid = spreading_activation_centroid(G, keywords)
+spreading_activation_centroid(G, keywords)
 
-for c in list(centroid.items())[:10]:
-    print(c)
-  
